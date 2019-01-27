7882a92df21a2bdf6732dae3c209b04122043e67
qiong.duan <super.duanqiong@gmail.com>
2019-01-27 23:56:33 +0800

    netty deploy time server

---
+++ src/main/java/org/dq/netty/netty/timeserver/TimeServer.java
@@ -0,0 +1,47 @@
+package org.dq.netty.netty.timeserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.sctp.nio.NioSctpServerChannel;
+import io.netty.channel.socket.SocketChannel;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class TimeServer {
+    public void bind(int port) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        try {
+            bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioSctpServerChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel channel) {
+                            channel.pipeline().addLast(new TimeServerHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int port = 8080;
+        if (!ArrayUtils.isEmpty(args)) {
+            try {
+
+                port = Integer.parseInt(args[0]);
+            } catch (NumberFormatException e) {
+            }
+        }
+        new TimeServer().bind(port);
+    }
+}
---
+++ src/main/java/org/dq/netty/netty/timeserver/TimeServerHandler.java
@@ -0,0 +1,7 @@
+package org.dq.netty.netty.timeserver;
+
+import io.netty.channel.ChannelHandlerAdapter;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+}
