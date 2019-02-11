66b882b7b931479d82e349e6e663902052b3c41f
qiong.duan <qiong.duan@langtaojin.com>
2019-02-11 17:01:34 +0800

    add log

--- src/main/java/org/dq/netty/netty/echo/server/EchoServerHandler.java
+++ src/main/java/org/dq/netty/netty/echo/server/EchoServerHandler.java
@@ -13,6 +13,10 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         ByteBuf in = (ByteBuf) msg;
+//        slice,duplicate,Unpooled.unmodifiableBuffer,order,readSlice,方法返回的ByteBuf，
+//          实例都指向了原有的实例，所以修改了当前值，原有的ByteBuf中的值会跟着改变，可以用copy方法生成新实例
+//        ByteBuf slice = ((ByteBuf) msg).slice();
+//        slice.setByte(0, 'J');
         System.out.println("Server received:  " + in.toString(CharsetUtil.UTF_8));//读取完成后，ByteBuf不会被释放
         ctx.write(in);
     }
