917c24cdc010012b537b17ef1281985d0966f446
qiong.duan <qiong.duan@langtaojin.com>
2019-02-14 18:44:06 +0800

    remove logfile

--- src/main/java/org/dq/netty/netty/udp/LogEvent.java
+++ src/main/java/org/dq/netty/netty/udp/LogEvent.java
@@ -1,4 +1,26 @@
 package org.dq.netty.netty.udp;
 
-public class LogEvent   {
+import lombok.Data;
+
+import java.net.InetSocketAddress;
+
+@Data
+public class LogEvent {
+    private static final byte SEPARATOR = ':';
+    private final InetSocketAddress source;
+    private final String logFile;
+    private final String msg;
+    private final long received;
+
+    public LogEvent(InetSocketAddress source, long received, String logFile, String msg) {
+        this.source = source;
+        this.logFile = logFile;
+        this.msg = msg;
+        this.received = received;
+    }
+
+    public LogEvent(String logFile, String msg) {
+        this(null, -1, logFile, msg);
+    }
+
 }
